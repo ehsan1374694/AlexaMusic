169726247c013b793dc52d93e85e6738c9b24ac2
ehsan1374694 <83925684+ehsan1374694@users.noreply.github.com>
2024-11-08 23:36:17 +0300

    Update stop.py

--- AlexaMusic/plugins/admins/stop.py
+++ AlexaMusic/plugins/admins/stop.py
@@ -22,13 +22,27 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 STOP_COMMAND = get_command("STOP_COMMAND")
 
 
-@app.on_message(filters.command(STOP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["end", "اتمام", "cend", "cstop"],prefixes=['']) & filters.group & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
-    await Alexa.stop_stream(chat_id)
+        return
+    await Onix.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
-        _["admin_9"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+@app.on_message(
+    filters.command(["شارژ"],prefixes=['']) & filters.group & ~BANNED_USERS
+)
+async def group_charge(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    month = message.command[1]
+    print('month',month)
+    await message.reply_text(
+        f"گروه با موفقیت {month} روز شارژ شد ."
     )
